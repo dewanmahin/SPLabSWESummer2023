75eab1cb479028e3349f3cb2fbd924baff422e85
DEWAN <dewanmahinchy@gmail.com>
2023-08-24 20:48:33 +0600

    problem-9.c added

---
+++ Basics/problem-9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char intValue = '5';
+    char charValue = 'A';
+    char charValue1 = 'a';
+    char symbolValue = ';';
+
+    printf("%d\n", intValue);
+    printf("%d\n", charValue);
+    printf("%d\n", charValue1);
+    printf("%d\n", symbolValue);
+
+    return 0;
+}
