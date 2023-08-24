82fa1e8d18271d0d148e36c94370630ba4985a0d
DEWAN <dewanmahinchy@gmail.com>
2023-08-24 23:12:03 +0600

    problem-23.c added

--- Basics/problem-23.c
+++ Basics/problem-23.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int n, d;
+    int n, d; // for example: n=53, d=3;
     scanf("%d %d", &n, &d);
+    // Quotient
+    int quotient = n / d; // output: 17
+    printf("Quotient: %d\n", quotient);
+    // Remainder (without using modulus(%) operator)
     int k;
     for(int i=0; ; i++){
         if(i*d >= n){
-            printf("%d\n", i);
             k = (i-1)*d;
             break;
         }
     }
-    int remainder = n - k;
-    printf("%d", remainder);
+    int remainder = n - k; // output: 2
+    printf("Remainder: %d", remainder);
 
     return 0;
 }
