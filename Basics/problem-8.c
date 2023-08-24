91a5681559a6c62e8cef191063044d7cf9c07dc7
DEWAN <dewanmahinchy@gmail.com>
2023-08-24 20:43:58 +0600

    problem-8.c added

---
+++ Basics/problem-8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    // Finding the size of different data types using sizeof() function
+    printf("Size of int data type: %d\n", sizeof(int));
+    printf("Size of long long int data type: %d\n", sizeof(long long int));
+    printf("Size of float data type: %d\n", sizeof(float));
+    printf("Size of double data type: %d\n", sizeof(double));
+    printf("Size of char data type: %d\n", sizeof(char));
+
+    return 0;
+}
