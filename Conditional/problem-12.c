d3d81bbae5141f2b41c1c9938680e479752aba92
DEWAN <dewanmahinchy@gmail.com>
2023-09-14 18:55:33 +0600

    problem-12.c added

---
+++ Conditional/problem-12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    // find largest number
+    int x,y,z;
+    scanf("%d %d %d", &x, &y, &z);
+
+    if(x >= y && x >= z){
+        printf("Largest Number: %d", x);
+    }else if(y >= x && y >= z){
+        printf("Largest Number: %d", y);
+    }else{
+        printf("Largest Number: %d", z);
+    }
+
+    return 0;
+}
