694f382e7651d411b4fe7c833fdb523ad0cb558d
DEWAN <dewanmahinchy@gmail.com>
2023-10-04 18:42:44 +0600

    comment added

--- Function_and_Recursion/problem-7.c
+++ Function_and_Recursion/problem-7.c
@@ -2,7 +2,7 @@
 int fibo(int n)
 {
     if (n == 0 || n == 1) return n; // Base case
-    
+    // Recursive call
     int f1 = fibo(n-1);
     int f2 = fibo(n-2);
     return f1 + f2;
@@ -13,7 +13,9 @@ int main()
     scanf("%d", &num); 
 
     int nthFibo = fibo(num);
-    printf("%d", nthFibo);
+    printf("%dth term Fibonacci: %d", num, nthFibo);
 
     return 0;
 }
+
+// C program to generate nth fibonacci term using recursion
