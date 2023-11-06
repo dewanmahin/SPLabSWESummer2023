90521334a714751d201bc7cb0fcbe1030bb5e843
DEWAN <dewanmahinchy@gmail.com>
2023-11-06 23:10:01 +0600

    problem-6.c file code updated

--- Function_and_Recursion/problem-6.c
+++ Function_and_Recursion/problem-6.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-int even_odd_recursion(int i, int n){
-    if(i > n) return 0;
-    if(i%2 == 0) printf("Even: %d\n", i);
-    else printf("Odd: %d\n", i);
-    return even_odd_recursion(++i, n);
+void even_odd_recursion(int i){
+    if(i==13) return; // base case
+
+    if(i%2 == 0)
+    {
+        printf("Even: %d\n", i);
+    }
+    else
+    {
+        printf("Odd: %d\n", i);
+    }
+
+    even_odd_recursion(i + 1);
 };
 int main()
 {
-    int n = 12;
     int i = 1;
-    even_odd_recursion(i, n);
+    even_odd_recursion(i);
 
     return 0;
 }
